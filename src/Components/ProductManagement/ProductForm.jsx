82d6373724ef3daff949890bbac8a1bf159146bf
Provash <provash20cb@gmail.com>
2025-10-03 21:33:21 +0600

    44-6 Display form data in a table and form error handle

--- src/Components/ProductManagement/ProductForm.jsx
+++ src/Components/ProductManagement/ProductForm.jsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const handleProductSubmit = (e) =>{
+
+const ProductForm = ({handleAddProduct}) => {
+
+    const [error, setError] = useState('')
+
+    const handleProductSubmit = (e) =>{
     e.preventDefault();
-    console.log(e.target)
+    // console.log(e.target)
 
     const name = e.target.name.value;
     const price = e.target.price.value;
     const quantity = e.target.quantity.value;
-    console.log(name, price, quantity);
+    // console.log(name, price, quantity);
+
+    if(name.length === 0){
+        setError("Please Probide Product Name");
+        return;
+    }
+    else if(price.length === 0){
+        setError("Please Probide Product Price");
+        return;
+    }
+    else if(price < 0){
+        setError("Price Can Not Be Negative");
+        return;
+    }
+    else if(quantity.length === 0){
+        setError("Please Probide Product Quantity");
+        return;
+    }
+    else{
+        setError("");
+    }
 
     const newProduct = {
         name,
         price,
         quantity
     }
-    console.log(newProduct);
+    // console.log(newProduct);
+    // use return
+    handleAddProduct(newProduct);  
+    // if(!error){
+    //     handleAddProduct(newProduct);
+    // }
 
 }
 
-const ProductForm = () => {
     return (
         <div>
             <h3>Add A Product</h3>
@@ -28,6 +57,7 @@ const ProductForm = () => {
                 <input type="text" name="quantity" placeholder="Product Quantity" /> <br />
                 <input type="submit" value="Submit" />
             </form>
+            <p style={{color:"red"}}><small>{error}</small></p>
         </div>
     );
 };
